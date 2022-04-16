ce41e1322d1a08e7dfddc0092ea8e11ae270c0e4
SthomsonPL <39528971+sthomsonpl@users.noreply.github.com>
2022-04-16 23:33:17 +0200

    Disable TPD and OPPerf

--- service.sh
+++ service.sh
@@ -6,3 +6,7 @@ done
 resetprop ro.boot.flash.locked 1
 resetprop ro.boot.vbmeta.device_state locked
 resetprop ro.boot.verifiedbootstate green
+chmod 000 /sys/module/tpd/parameters/tpd_cmds
+chmod 000 /sys/module/tpd/parameters/tpd_dynamic
+chmod 000 /sys/module/tpd/parameters/tpd_enable
+chmod 000 /sys/module/tpd/parameters/tpd_id
